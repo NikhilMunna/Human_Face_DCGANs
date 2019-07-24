5a28365814e56023b608c224101126a76070684a
NikhilMunna <nikhil.munna001@gmail.com>
2019-07-24 17:39:18 +0530

    modifying images

--- dcgan.py
+++ dcgan.py
@@ -18,3 +18,15 @@ if not os.path.isdir(directory):
     tar = tarfile.open(filename, "r:gz")
     tar.extractall(path=directory)
     tar.close()
+
+filepaths = []
+for dir_, _, files in os.walk(directory):
+    for fileName in files:
+        relDir = os.path.relpath(dir_, directory)
+        relFile = os.path.join(relDir, fileName)
+        filepaths.append(directory + "/" + relFile)
+        
+for i, fp in enumerate(filepaths):
+    img = imread(fp) #/ 255.0
+    img = imresize(img, (40, 40))
+    imsave(new_dir + "/" + str(i) + ".png", img)
